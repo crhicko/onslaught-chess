1ff6bc4cc5d4294648d191098b518ce7e8650c84
chickok <crhicko@g.clemson.edu>
2019-08-13 22:25:10 -0400

    removed comments

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -25,9 +25,6 @@ class Board extends React.Component {
 
     constructor(){
         super();
-
-
-
         //TODO: refactor this, kinda messy
         for(let i = 0; i < 8; i++)
             for(let j = 0 ; j < 8; j++){
@@ -84,15 +81,8 @@ class Board extends React.Component {
                 })
             });
         }
-
         this.setState({ userActionSelect: this.state.userActionSelect === 'piece' ? 'tile' : "piece"});
     }
-
-    // checkIfTileMovable(tile, curTile){
-    //     if(tile.piece.color == curTile.piece.color)
-    // }
-    //
-    setMovableAndUpCount
 
     setMovableTiles = (tile) => {
         let curTile;
@@ -235,105 +225,9 @@ class Board extends React.Component {
             default:
                 break;
         }
-
         console.log(tileCount);
         return tileCount;
     }
-
-    //runs on every click of a piece
-    /*
-    setHighlightedTiles = (piece, col_base, row_base) => {
-        console.log(piece);
-        console.log(col_base);
-        console.log(row_base);
-
-        //reset highlightedTiles
-        this.highlightedTiles = [];
-
-        switch(piece) {
-            case "pawn":
-                break;
-            case "bishop":
-                //TODO: Refactor this code into modularized functions
-                for(let boundCount = 0; boundCount < 4; boundCount++){
-                    switch(boundCount) {
-                        //TODO ENUM THESE
-                        case 0: //upleft -col - row
-                            for(let i = 1; col_base - i >= 0 && row_base - i >= 0; i++){
-                                if(this.boardPieces[(col_base - i) + (row_base - i) * 8] !== 'none'){
-                                    this.highlightedTiles.push({x: col_base - i, y: row_base - i})
-                                    break;
-                                }
-                                else
-                                    this.highlightedTiles.push({x: col_base - i, y: row_base - i})
-                            }
-                            break;
-                        case 1: // upright
-                            for(let i = 1; col_base + i <= 7 && row_base - i >= 0; i++){
-                                if(this.boardPieces[(col_base + i) + (row_base - i) * 8] !== 'none'){
-                                    this.highlightedTiles.push({x: col_base + i, y: row_base - i})
-                                    break;
-                                }
-                                else
-                                    this.highlightedTiles.push({x: col_base + i, y: row_base - i})
-                            }
-                            break;
-                        case 2: //downright
-                            for(let i = 1; col_base + i <= 7 && row_base + i <= 7; i++){
-                                if(this.boardPieces[(col_base + i) + (row_base + i) * 8] !== 'none'){
-                                    this.highlightedTiles.push({x: col_base + i, y: row_base + i})
-                                    break;
-                                }
-                                else
-                                    this.highlightedTiles.push({x: col_base + i, y: row_base + i})
-                            }
-                            break;
-                        case 3: //downleft
-                            for(let i = 1; col_base - i >= 0 && row_base + i <= 7; i++){
-                                if(this.boardPieces[(col_base - i) + (row_base + i) * 8] !== 'none'){
-                                    this.highlightedTiles.push({x: col_base - i, y: row_base + i})
-                                    break;
-                                }
-                                else
-                                    this.highlightedTiles.push({x: col_base - i, y: row_base + i})
-                            }
-                            break;
-                        default:
-                    }
-                }
-                break;
-            case "knight":
-                //TODO: knight movement is messy
-
-                for(let count = 0; count < 8 ; count++){
-
-                }
-                console.log(this.getBoardPiece(col_base,row_base))
-                if(col_base >= 2)
-                if(col_base <= 5){
-
-                }
-                if(row_base >= 2)
-                if(row_base <= 5){
-                    if(this.getBoardPiece(col_base + 2, row_base - 1))
-                        this.highlightedTiles.push({x: col_base + 2, y: row_base - 1})
-                    if(this.getBoardPiece(col_base + 2, row_base + 1))
-                        this.highlightedTiles.push({x: col_base + 2, y: row_base + 1})
-                }
-
-                break;
-            case "rook":
-                break;
-            case "queen":
-                break;
-            case "king":
-                break;
-            default:
-        }
-
-        this.forceUpdate();
-    }
-    */
 
     getIfTileIsHighlighted = (i) => {
         let result = false;
